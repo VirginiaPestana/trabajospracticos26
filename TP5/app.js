be533667499fe929b5329ef1b51776b555535c02
alumno <52211295@ivt.edu.ar>
2026-06-11 10:41:57 -0300

    12

--- TP5/app.js
+++ TP5/app.js
@@ -15,7 +15,20 @@ alert('La resta es: ' + resta)
 let multiplicacion
 multiplicacion= numero1 * numero2
 console.log('La multiplicacion es: ' + multiplicacion)
+alert('La multiplicacion es: ' + multiplicacion)
 
 let division 
 division= numero1 / numero2
 console.log('La division es: ' + division)
+alert('La division es: ' + division)
+
+let nombreAlumno
+nombreAlumno= Virginia
+
+let apellidoAlumno
+apellidoAlumno= Pestana
+
+let edadAlumno
+edadAlumno= 14
+
+console.log('El alumno:' + nombreAlumno + apellidoAlumno  ' tiene '+ edadAlumno)
