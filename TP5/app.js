ba4975acdce6c44451d6b7414807fb7e9f40a284
alumno <52211295@ivt.edu.ar>
2026-06-11 10:26:40 -0300

    11

---
+++ TP5/app.js
@@ -0,0 +1,21 @@
+let numero1= 4
+
+let numero2= 2
+
+let suma
+suma= numero1 + numero2
+console.log('La suma es: ' + suma)
+alert('La suma es: ' + suma)
+
+let resta
+resta= numero1 - numero2
+console.log('La resta es: ' + resta)
+alert('La resta es: ' + resta)
+
+let multiplicacion
+multiplicacion= numero1 * numero2
+console.log('La multiplicacion es: ' + multiplicacion)
+
+let division 
+division= numero1 / numero2
+console.log('La division es: ' + division)
